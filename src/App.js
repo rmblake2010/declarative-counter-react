85fc6c251c61adb5782a52b2400435168115aeb1
Ryan Blake <xbungalowx@gmail.com>
2022-03-19 14:23:51 -0500

    deleted original code

--- src/App.js
+++ src/App.js
@@ -7,22 +7,8 @@ function App() {
   window.changeCounter = changeCounter
   return (
     <div className="App">
-      <header className="App-header">
         <h1>{counter}</h1>
         <h4>{counter}</h4>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
